c4edb5f96c9b16d1a9befcec31e56c9fef93c0ef
mpqm <okqkrwhdtjd@gmail.com>
2024-08-23 11:27:49 +0900

    [test] test02

--- src/main/java/com/devops/jenkins/test/TestController.java
+++ src/main/java/com/devops/jenkins/test/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("test01");
+        return ResponseEntity.ok("test02");
     }
 }
